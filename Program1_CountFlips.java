1e0c857b5eabf51d07e5f6fad354032b725c585e
Kayton Melchoir <sahidmelchoir@gmail.com>
2026-09-23 19:02:26 -0400

    Flipping Heads or Tails + Camel Casing + Importing Random
    Imported Random.
    
    Modified variable names with Camel Casing.
    
    Changed StartingValue to flipAttempt
    Added For Loop using flipAttempt.
    
    Added if the next random int is 0, increment heads, else, increment tails.

--- Program1_CountFlips.java
+++ Program1_CountFlips.java
@@ -1,10 +1,19 @@
+import java.util.Random;
 public class Program1_CountFlips {
   public static void main(String[] args) {
-    int Heads = 0;
-    int Tails = 0;
-    int Max = 100;
-    int StartingValue = 0;
+    Random random = new Random();
 
+    int heads = 0;
+    int tails = 0;
+    int max = 100;
+    int flipAttempt = 0;
 
+    for(flipAttempt; flipAttempt < 100; flipAttempt++){
+      if (random.nextInt(2) == 0){
+        heads++;
+      } else{
+        tails++;
+      }
+    }
   }
 }
