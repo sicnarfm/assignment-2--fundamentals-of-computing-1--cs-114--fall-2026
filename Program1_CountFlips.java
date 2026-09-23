a0ce6a13917f4df41cf52a541252ebb128efb79a
Kayton Melchoir <sahidmelchoir@gmail.com>
2026-09-23 19:30:21 -0400

    Removed line 8.
    int max removed due to not needing it.

--- Program1_CountFlips.java
+++ Program1_CountFlips.java
@@ -3,9 +3,8 @@ public class Program1_CountFlips {
   public static void main(String[] args) {
     Random random = new Random();
 
-    int heads = 0;
-    int tails = 0;
-    int max = 100;
+    int headsCount = 0;
+    int tailsCount = 0;
     int flipAttempt;
 
     for(flipAttempt = 0; flipAttempt < 100; flipAttempt++){
