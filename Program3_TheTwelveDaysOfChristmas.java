f8c955d232e657660274dadaea5374bcb430dece
Kayton Melchoir <sahidmelchoir@gmail.com>
2026-09-24 17:33:27 -0400

    Added a for loop that outputs the lyrics in order.

--- Program3_TheTwelveDaysOfChristmas.java
+++ Program3_TheTwelveDaysOfChristmas.java
@@ -2,6 +2,45 @@ public class Program3_TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
     int days;
 
-
+ for(days = 0; days < 13; days++){
+      switch(days){
+        case 1:
+          System.out.println("On the first day of Christmas, my true love sent to me\nA partridge in a pear tree");
+          break;
+        case 2:
+          System.out.println("On the second day of Christmas, my true love sent to me\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 3:
+          System.out.println("On the third day of Christmas, my true love sent to me\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 4:
+          System.out.println("On the fourth day of Christmas, my true love sent to me\nFour calling birds\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 5:
+          System.out.println("On the fifth day of Christmas, my true love sent to me\nFive golden rings\nFour calling birds\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 6:
+          System.out.println("On the sixth day of Christmas, my true love sent to me\nSix geese a-laying\nFive golden rings\nFour calling birds\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 7:
+          System.out.println("On the seventh day of Christmas, my true love sent to me\nSeven swans a-swimming\nSix geese a-laying\nFive golden rings\nFour calling birds\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 8:
+          System.out.println("On the eighth day of Christmas, my true love sent to me\nEight maids a-milking\nSeven swans a-swimming\nSix geese a-laying\nFive golden rings\nFour calling birds\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 9:
+          System.out.println("On the ninth day of Christmas, my true love sent to me\nNine ladies dancing\nEight maids a-milking\nSeven swans a-swimming\nSix geese a-laying\nFive golden rings\nFour calling birds\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 10:
+          System.out.println("On the tenth day of Christmas, my true love sent to me\nTen lords a-leaping\nNine ladies dancing\nEight maids a-milking\nSeven swans a-swimming\nSix geese a-laying\nFive golden rings\nFour calling birds\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 11:
+          System.out.println("On the eleventh day of Christmas, my true love sent to me\nEleven pipers piping\nTen lords a-leaping\nNine ladies dancing\nEight maids a-milking\nSeven swans a-swimming\nSix geese a-laying\nFive golden rings\nFour calling birds\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+        case 12:
+          System.out.println("On the twelfth day of Christmas, my true love sent to me\nTwelve drummers drumming\nEleven pipers piping\nTen lords a-leaping\nNine ladies dancing\nEight maids a-milking\nSeven swans a-swimming\nSix geese a-laying\nFive golden rings\nFour calling birds\nThree french hens\nTwo turtle doves and\nA partridge in a pear tree");
+          break;
+      }
+    }
   }
 }
